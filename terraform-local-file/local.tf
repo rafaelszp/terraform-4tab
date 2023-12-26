263e8259faed4fe2fae24d82fa52fd1887f8ceb5
Rafael Pereira | SENAI <rafaelsouza@fieg.com.br>
2023-12-26 12:46:30 -0300

    First project

---
+++ terraform-local-file/local.tf
@@ -0,0 +1,12 @@
+# resource = block name
+# "local_file" = provider + resource type; local = provider, file = resource type
+# "pet" = resource name
+resource "local_file" "pet" {
+
+  #Argument filename
+  filename = "files/pets.txt"
+  #Argument content
+  content = "All the pets are petsy"
+  # file_permission = "0666"
+  directory_permission = "0666"
+}
