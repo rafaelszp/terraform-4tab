15268f407fe02c565b1c54dda1a6db98cb8f2e3b
Rafael Pereira | SENAI <rafaelsouza@fieg.com.br>
2024-02-16 15:08:32 -0300

    format

--- 14-dynamodb-01/main.tf
+++ 14-dynamodb-01/main.tf
@@ -1,15 +1,15 @@
 data "template_file" "cars_json_template" {
-  template = "${file("${path.module}/templates/cars.json.tpl")}"
-  vars     = {
-    schema_version  = var.schema_version
+  template = file("${path.module}/templates/cars.json.tpl")
+  vars = {
+    schema_version = var.schema_version
   }
 }
 
 resource "aws_dynamodb_table" "cars" {
-  name = "cars"
-  hash_key = "RENAVAM"
+  name         = "cars"
+  hash_key     = "RENAVAM"
   billing_mode = "PAY_PER_REQUEST"
-  range_key = "Make"
+  range_key    = "Make"
   attribute {
     name = "RENAVAM"
     type = "S" #string
@@ -21,8 +21,8 @@ resource "aws_dynamodb_table" "cars" {
 }
 
 resource "aws_dynamodb_table_item" "car-items" {
-  table_name    = aws_dynamodb_table.cars.name
-  hash_key      = aws_dynamodb_table.cars.hash_key
-  range_key     = aws_dynamodb_table.cars.range_key
-  item =  data.template_file.cars_json_template.rendered
+  table_name = aws_dynamodb_table.cars.name
+  hash_key   = aws_dynamodb_table.cars.hash_key
+  range_key  = aws_dynamodb_table.cars.range_key
+  item       = data.template_file.cars_json_template.rendered
 }
--- 14-dynamodb-01/provider.tf
+++ 14-dynamodb-01/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
-  region                      = "us-east-1"
+  access_key = "mock_access_key"
+  secret_key = "mock_secret_key"
+  region     = "us-east-1"
 
   s3_use_path_style           = true
   skip_credentials_validation = true
